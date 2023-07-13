97888278b173716d865183c4abd0e53394bf0452
SaulRC1 <saul.rodriguez.naranjo@gmail.com>
2023-07-13 23:30:14 +0200

    Setting up Saluhud project

---
+++ src/main/java/com/uhu/saluhud/SaluhudApplication.java
@@ -0,0 +1,13 @@
+package com.uhu.saluhud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaluhudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaluhudApplication.class, args);
+	}
+
+}
