a2e236f69ad4513771664191e1ca3d2fc9a20608
Juan2k <jadova16@gmail.com>
2025-04-14 21:52:36 +0200

    fix(NutritionLocaleProvider): Añadido codificación charset UTF-8 para los ficheros de internacionalización.

--- src/main/java/com/uhu/saluhud/localization/NutritionLocaleProvider.java
+++ src/main/java/com/uhu/saluhud/localization/NutritionLocaleProvider.java
@@ -1,7 +1,10 @@
 package com.uhu.saluhud.localization;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -22,11 +25,11 @@ import org.springframework.stereotype.Component;
 public class NutritionLocaleProvider
 {
     private static final Logger logger = Logger.getLogger(NutritionLocaleProvider.class.getName());
-    
+
     private final List<Locale> supportedLocales;
 
     private String translationsRootFolder;
-    
+
     public static final String RECIPES_TRANSLATION_BUNDLE_PREFIX = "recipes";
     public static final String INGREDIENTS_TRANSLATION_BUNDLE_PREFIX = "ingredients";
     public static final String RECIPE_ELABORATION_STEPS_TRANSLATION_BUNDLE_PREFIX = "recipeElaborationSteps";
@@ -38,134 +41,146 @@ public class NutritionLocaleProvider
     {
         logger.log(Level.INFO, "Nutrition translations being loaded from: \"{0}\"", translationsRootFolder);
         this.translationsRootFolder = translationsRootFolder;
-        
+
         supportedLocales = Arrays.asList(Locale.ENGLISH, Locale.of("es"));
         translationBundles = new HashMap<>();
-        
+
         for (Locale supportedLocale : supportedLocales)
         {
-            String recipesTranslationsBundlePath = this.translationsRootFolder + File.separator 
+            String recipesTranslationsBundlePath = this.translationsRootFolder + File.separator
                     + RECIPES_TRANSLATION_BUNDLE_PREFIX + "_" + supportedLocale.toLanguageTag() + ".properties";
-            
-            logger.log(Level.INFO, "Loading recipes translations bundle for locale \"{0}\" from: \"{1}\"", new String[] {supportedLocale.toLanguageTag(), 
-                recipesTranslationsBundlePath});
-            
-            try(FileInputStream input = new FileInputStream(recipesTranslationsBundlePath))
+
+            logger.log(Level.INFO, "Loading recipes translations bundle for locale \"{0}\" from: \"{1}\"", new String[]
             {
+                supportedLocale.toLanguageTag(),
+                recipesTranslationsBundlePath
+            });
+
+            try (InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(recipesTranslationsBundlePath), 
+                    StandardCharsets.UTF_8); BufferedReader reader = new BufferedReader(inputStreamReader))
+            {
+
                 Properties recipesTranslationsBundle = new Properties();
-                recipesTranslationsBundle.load(input);
-                
+                recipesTranslationsBundle.load(reader);
+
                 translationBundles.put(RECIPES_TRANSLATION_BUNDLE_PREFIX + "_" + supportedLocale.toLanguageTag(), recipesTranslationsBundle);
-                
+
                 logger.log(Level.INFO, "Loaded recipes translations bundle for locale \"{0}\" succesfully", supportedLocale.toLanguageTag());
-            }
-            catch(Exception e)
+            } catch (Exception e)
             {
-                //logger.log(Level.SEVERE, e.getMessage(), e);
                 throw e;
             }
-            
-            String ingredientsTranslationsBundlePath = this.translationsRootFolder + File.separator 
+
+            String ingredientsTranslationsBundlePath = this.translationsRootFolder + File.separator
                     + INGREDIENTS_TRANSLATION_BUNDLE_PREFIX + "_" + supportedLocale.toLanguageTag() + ".properties";
-            
-            logger.log(Level.INFO, "Loading ingredients translations bundle for locale \"{0}\" from: \"{1}\"", new String[] {supportedLocale.toLanguageTag(), 
-                ingredientsTranslationsBundlePath});
-            
-            try(FileInputStream input = new FileInputStream(ingredientsTranslationsBundlePath))
+
+            logger.log(Level.INFO, "Loading ingredients translations bundle for locale \"{0}\" from: \"{1}\"", new String[]
             {
+                supportedLocale.toLanguageTag(),
+                ingredientsTranslationsBundlePath
+            });
+
+            try (InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(ingredientsTranslationsBundlePath), 
+                    StandardCharsets.UTF_8); BufferedReader reader = new BufferedReader(inputStreamReader))
+            {
+
                 Properties ingredientsTranslationsBundle = new Properties();
-                ingredientsTranslationsBundle.load(input);
-                
+                ingredientsTranslationsBundle.load(reader);
+
                 translationBundles.put(INGREDIENTS_TRANSLATION_BUNDLE_PREFIX + "_" + supportedLocale.toLanguageTag(), ingredientsTranslationsBundle);
-                
+
                 logger.log(Level.INFO, "Loaded ingredients translations bundle for locale \"{0}\" succesfully", supportedLocale.toLanguageTag());
-            }
-            catch(Exception e)
+            } catch (Exception e)
             {
-                //logger.log(Level.SEVERE, e.getMessage(), e);
                 throw e;
             }
-            
-            String recipeElaborationStepsTranslationsBundlePath = this.translationsRootFolder + File.separator 
+
+            String recipeElaborationStepsTranslationsBundlePath = this.translationsRootFolder + File.separator
                     + RECIPE_ELABORATION_STEPS_TRANSLATION_BUNDLE_PREFIX + "_" + supportedLocale.toLanguageTag() + ".properties";
-            
-            logger.log(Level.INFO, "Loading recipe elaboration steps translations bundle for locale \"{0}\" from: \"{1}\"", new String[] {supportedLocale.toLanguageTag(), 
-                recipeElaborationStepsTranslationsBundlePath});
-            
-            try(FileInputStream input = new FileInputStream(recipeElaborationStepsTranslationsBundlePath))
+
+            logger.log(Level.INFO, "Loading recipe elaboration steps translations bundle for locale \"{0}\" from: \"{1}\"", new String[]
             {
+                supportedLocale.toLanguageTag(),
+                recipeElaborationStepsTranslationsBundlePath
+            });
+
+            try (InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(recipeElaborationStepsTranslationsBundlePath), 
+                    StandardCharsets.UTF_8); BufferedReader reader = new BufferedReader(inputStreamReader))
+            {
+
                 Properties recipeElaborationStepsTranslationsBundle = new Properties();
-                recipeElaborationStepsTranslationsBundle.load(input);
-                
+                recipeElaborationStepsTranslationsBundle.load(reader);
+
                 translationBundles.put(RECIPE_ELABORATION_STEPS_TRANSLATION_BUNDLE_PREFIX + "_" + supportedLocale.toLanguageTag(), recipeElaborationStepsTranslationsBundle);
-                
+
                 logger.log(Level.INFO, "Loaded recipe elaboration steps translations bundle for locale \"{0}\" succesfully", supportedLocale.toLanguageTag());
-            }
-            catch(Exception e)
+            } catch (Exception e)
             {
-                //logger.log(Level.SEVERE, e.getMessage(), e);
                 throw e;
             }
-            
-            String recipeIngredientTranslationsBundlePath = this.translationsRootFolder + File.separator 
+
+            String recipeIngredientTranslationsBundlePath = this.translationsRootFolder + File.separator
                     + RECIPE_INGREDIENT_TRANSLATION_BUNDLE_PREFIX + "_" + supportedLocale.toLanguageTag() + ".properties";
-            
-            logger.log(Level.INFO, "Loading recipe ingredient translations bundle for locale \"{0}\" from: \"{1}\"", new String[] {supportedLocale.toLanguageTag(), 
-                recipeIngredientTranslationsBundlePath});
-            
-            try(FileInputStream input = new FileInputStream(recipeIngredientTranslationsBundlePath))
+
+            logger.log(Level.INFO, "Loading recipe ingredient translations bundle for locale \"{0}\" from: \"{1}\"", new String[]
             {
+                supportedLocale.toLanguageTag(),
+                recipeIngredientTranslationsBundlePath
+            });
+
+            try (InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(recipeIngredientTranslationsBundlePath), StandardCharsets.UTF_8); 
+                    BufferedReader reader = new BufferedReader(inputStreamReader))
+            {
+
                 Properties recipeIngredientTranslationsBundle = new Properties();
-                recipeIngredientTranslationsBundle.load(input);
-                
-                translationBundles.put(INGREDIENTS_TRANSLATION_BUNDLE_PREFIX + "_" + supportedLocale.toLanguageTag(), recipeIngredientTranslationsBundle);
-                
+                recipeIngredientTranslationsBundle.load(reader);
+
+                translationBundles.put(RECIPE_INGREDIENT_TRANSLATION_BUNDLE_PREFIX + "_" + supportedLocale.toLanguageTag(), recipeIngredientTranslationsBundle);
+
                 logger.log(Level.INFO, "Loaded recipe ingredient translations bundle for locale \"{0}\" succesfully", supportedLocale.toLanguageTag());
-            }
-            catch(Exception e)
+            } catch (Exception e)
             {
-                //logger.log(Level.SEVERE, e.getMessage(), e);
                 throw e;
             }
         }
     }
-    
+
     public List<Locale> getSupportedLocales()
     {
         return this.supportedLocales;
     }
-    
+
     public boolean isLocaleSupported(Locale locale)
     {
         return this.supportedLocales.contains(locale);
     }
 
     /**
-     * Gets the translation for the key specified by parameter within the specified
-     * bundle and locale.
-     * 
+     * Gets the translation for the key specified by parameter within the
+     * specified bundle and locale.
+     *
      * @param key The key for the desired translation.
-     * @param bundle The bundle name where the key resides. Must be only the 
+     * @param bundle The bundle name where the key resides. Must be only the
      * bundle's base name.
      * @param locale The desired translation's locale.
-     * 
-     * @return The translation for the specified key, within the specified bundle,
-     * in the desired locale.
+     *
+     * @return The translation for the specified key, within the specified
+     * bundle, in the desired locale.
      */
     public String getTranslation(String key, String bundle, Locale locale)
     {
-        if(!isLocaleSupported(locale))
+        if (!isLocaleSupported(locale))
         {
             throw new IllegalArgumentException("Locale " + locale.toString() + " is not supported");
         }
-        
+
         Properties translationBundle = translationBundles.get(bundle + "_" + locale.toLanguageTag());
-        
-        if(translationBundle == null)
+
+        if (translationBundle == null)
         {
             throw new IllegalArgumentException("Bundle '" + bundle + "' not found");
         }
-        
+
         return (String) translationBundle.get(key);
     }
 }
