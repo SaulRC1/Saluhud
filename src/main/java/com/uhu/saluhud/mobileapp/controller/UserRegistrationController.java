9044155b9a8abdd36c05e910cce898e67ead7dc9
SaulRC1 <saul.rodriguez.naranjo@gmail.com>
2025-06-10 02:12:57 +0200

    fix(UserRegistrationController): Added historicals creation when an user registers

--- src/main/java/com/uhu/saluhud/mobileapp/controller/UserRegistrationController.java
+++ src/main/java/com/uhu/saluhud/mobileapp/controller/UserRegistrationController.java
@@ -4,11 +4,18 @@ import com.uhu.saluhud.mobileapp.dto.user.SaluhudUserRegistrationDTO;
 import com.uhu.saluhud.mobileapp.localization.MobileAppLocaleProvider;
 import com.uhu.saluhud.mobileapp.response.ApiErrorResponse;
 import com.uhu.saluhud.mobileapp.response.ApiInformationResponse;
+import com.uhu.saluhuddatabaseutils.models.user.DailyStepsHistorical;
 import com.uhu.saluhuddatabaseutils.models.user.SaluhudUser;
+import com.uhu.saluhuddatabaseutils.models.user.SleepHistorical;
+import com.uhu.saluhuddatabaseutils.models.user.WeightHistorical;
+import com.uhu.saluhuddatabaseutils.services.mobileapp.user.MobileAppDailyStepsHistoricalService;
 import com.uhu.saluhuddatabaseutils.services.mobileapp.user.MobileAppSaluhudUserService;
+import com.uhu.saluhuddatabaseutils.services.mobileapp.user.MobileAppSleepHistoricalService;
+import com.uhu.saluhuddatabaseutils.services.mobileapp.user.MobileAppWeightHistoricalService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import java.util.Locale;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -32,6 +39,15 @@ public class UserRegistrationController
     
     @Autowired
     private MobileAppLocaleProvider mobileAppLocaleProvider;
+    
+    @Autowired
+    private MobileAppDailyStepsHistoricalService mobileAppDailyStepsHistoricalService;
+    
+    @Autowired
+    private MobileAppSleepHistoricalService mobileAppSleepHistoricalService;
+    
+    @Autowired
+    private MobileAppWeightHistoricalService mobileAppWeightHistoricalService;
     
     @PostMapping
     public ResponseEntity<Object> registerSaluhudUser(@RequestBody @Valid SaluhudUserRegistrationDTO saluhudUserRegistrationDTO, HttpServletRequest request)
@@ -70,6 +86,26 @@ public class UserRegistrationController
                 saluhudUserRegistrationDTO.getName(), saluhudUserRegistrationDTO.getSurname(), saluhudUserRegistrationDTO.getPhoneNumber());
         
         mobileAppSaluhudUserService.registerSaluhudUser(saluhudUser);
+        
+        Optional<SaluhudUser> savedUserOptional = this.mobileAppSaluhudUserService.findByUsername(saluhudUser.getUsername());
+        
+        if(savedUserOptional.isPresent())
+        {
+            WeightHistorical weightHistorical = new WeightHistorical();
+            weightHistorical.setUser(savedUserOptional.get());
+            
+            this.mobileAppWeightHistoricalService.save(weightHistorical);
+            
+            SleepHistorical sleepHistorical = new SleepHistorical();
+            sleepHistorical.setUser(savedUserOptional.get());
+            
+            this.mobileAppSleepHistoricalService.save(sleepHistorical);
+            
+            DailyStepsHistorical dailyStepsHistorical = new DailyStepsHistorical();
+            dailyStepsHistorical.setUser(savedUserOptional.get());
+            
+            this.mobileAppDailyStepsHistoricalService.save(dailyStepsHistorical);
+        }
         
         return ResponseEntity.ok(new ApiInformationResponse(request.getServletPath(), 
                 mobileAppLocaleProvider.getTranslation("registration.userRegisteredSuccessfully", 
